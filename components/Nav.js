83a1e265d9ab649877eb8e159c8126e539ec26dd
Tomoki P <tmptsh@gmail.com>
2021-09-28 20:17:01 -0500

    add bio, gallery pages

---
+++ components/Nav.js
@@ -0,0 +1,8 @@
+<nav>
+<i class="fas fa-bars"></i>
+<ul class="hidden--mobile nav-links">
+  <li><a href="bio.html">Bio</a></li>
+  <li><a href= "gallery.html">Gallery</a></li>
+  <li><a href="register.html">Register</a></li>
+</ul>
+</nav>
