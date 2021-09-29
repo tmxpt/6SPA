e64ffb161536c33c9bd0972a3b991467b0fa9f19
Tomoki P <tmptsh@gmail.com>
2021-09-28 21:54:54 -0500

    works

--- components/views/index.js
+++ components/views/index.js
@@ -1,3 +1,3 @@
 export { default as Home } from "./Home.js";
-export { default as GalleryHome } from "./GalleryHome.js";
+export { default as Gallery } from "./Gallery.js";
 export { default as Bio } from "./Bio.js";
