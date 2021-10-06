02e5b0868b88b438f20a3be71de15dc2d14b9022
Tomoki P <tmptsh@gmail.com>
2021-10-05 21:42:35 -0500

    add weather add pizza

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 export { default as Bio } from "./Bio";
 export { default as Gallery } from "./Gallery";
 export { default as Home } from "./Home";
+export { default as Pizza } from "./Pizza";
 export { default as Links } from "./Links";
